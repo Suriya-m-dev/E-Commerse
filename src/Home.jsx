99cdefcd07a9675fe547751eec3fa0808e99b59e
Suriya M <suriyamuruges@gmail.com>
2025-04-26 17:44:41 +0530

    Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [sampleUser, setSampleUser] = useState({
+    email: "suriya123@gmail.com",
+    password: "Suriya@123",
+  });
+  const navigate = useNavigate();
+
+  const buttonClick = () => {
+    if (email === sampleUser.email && password === sampleUser.password) {
+      navigate("/product");
+      // alert("Correct");
+    } else {
+      alert("Email and Password Not Valid");
+    }
+  };
+
+  return (
+    <>
+      <div className="login-container">
+        <form className="login-form">
+          <div>
+            <h2>Login</h2>
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password" 
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="button" type="submit" onClick={buttonClick}>
+            Login
+          </button>
+        </form>
+      </div>
+      <div>hi</div>
+    </>
+  );
+}
+
+export default Home;
